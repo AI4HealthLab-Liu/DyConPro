ad0d0b1de009ab3429ea6ed6d5b4fc15523b05d5
Michael Tobia <33167922+tobiamj@users.noreply.github.com>
2018-12-28 15:52:57 -0500

    Update dcp_ifc.m

--- dcp_ifc.m
+++ dcp_ifc.m
@@ -6,23 +6,13 @@ function [ifc]=dcp_ifc(x)
 
 [row,col]=size(x);
 [x1,xfr,~]=dcp_mirror_pad(x);
-% ampx=abs(hilbert(x1));
-% ampx=ampx(xfr+1:xfr+row,:);
+
 phx=angle(hilbert(x1));
 phx=phx(xfr+1:xfr+row,:);
 ifc=zeros(row,col,col);
-% ampmod=zeros(row,col,col);
-% ampx=ampx./norm(ampx);
-% phimod=zeros(row,col,col);
-% phx=phx./norm(phx);
+
 for slice=1:col
     ifc(:,:,slice)=bsxfun(@minus,phx(:,slice),phx);
-%     ifca=bsxfun(@minus,phx(:,slice),phx);
-%     ifcb=bsxfun(@plus,phx(:,slice),phx);
-%     amp1=bsxfun(@minus,ampx(:,slice),ampx);
-%     amp2=bsxfun(@plus,ampx(:,slice),ampx);
-%     ampmod(:,:,slice)=1-(abs(amp1)./amp2);
-%     phimod(:,:,slice)=1-((ifca)./ifcb);
 end
 ifc=cos(ifc);
 
